b35cd48ee2b48e6166e1074c32561937347a9c30
sirkadogan <thaleskenne@hotmail.com>
2020-12-02 18:25:23 -0300

    Created layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'primeflex/primeflex.css';
 import 'primereact/resources/themes/arya-orange/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
+import './index.css';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <Dashboard />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
-const Dashboard = () => {
+import { Sidebar } from 'primereact/sidebar';
+import { Menu } from 'primereact/menu';
+import { Panel } from 'primereact/panel';
+
+import styles from './styles.module.css';
+
+const items = [
+  { label: 'New', icon: 'pi pi-fw pi-plus' },
+  { label: 'Delete', icon: 'pi pi-fw pi-trash' },
+];
+
+const Dashboard: React.SFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Setup</p>
-      </header>
+    <div className="p-grid">
+      <div className={`p-col-12 ${styles.header}`} />
+      <div className={`p-col-2 ${styles['sidebar-container']}`} />
+      <div className={`p-col-10 ${styles.content}`} />
     </div>
   );
-}
+};
 
 export default Dashboard;
