a89b03fef3455957b7ea6bc6b1224b4e717636df
sirkadogan <thaleskenne@hotmail.com>
2020-12-01 21:11:18 -0300

    Initial commit

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Setup</p>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
