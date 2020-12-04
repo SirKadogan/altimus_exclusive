95a3efe267f896ddb18aee992c01189f6f77d904
sirkadogan <thaleskenne@hotmail.com>
2020-12-04 09:59:29 -0300

    ADded logo to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Assets
+import Logo from '../../assets/logo.png';
+
 import styles from './styles.module.css';
 
 interface HeaderProps {
@@ -8,12 +11,15 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ toggleSideBar }: HeaderProps) => {
   return (
-    <div className={`p-col-12 ${styles.header} p-ai-center p-d-md-none`}>
+    <div
+      className={`p-col-12 ${styles.header} p-d-flex p-ai-center p-d-md-none`}
+    >
       <i
         className="pi pi-bars p-ml-2 p-d-md-none"
         style={{ fontSize: '2em', color: 'rgba(255, 255, 255, 0.6)' }}
         onClick={() => toggleSideBar()}
       />
+      <img className="p-ml-2" src={Logo} alt="logo" height={60} />
     </div>
   );
 };
